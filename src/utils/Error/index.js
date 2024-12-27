b3eaefd1217e128f440de4d931b41e35c1558469
Sharjeel Faiq <mrmalik610@gmail.com>
2024-12-27 21:51:50 +0500

    Replace console.error with logger.error in handleError utility for improved error logging

--- src/utils/Error/index.js
+++ src/utils/Error/index.js
@@ -1,10 +1,13 @@
 import { createError } from '#packages/index.js';
+import { logger } from '#utils/index.js';
 
 export const handleError = (error, message) => {
-  console.error('Error: ', error);
+  logger.error('Error: ', error);
+
   if (!error.status) {
     error = createError(500, message);
   }
+
   return {
     status: error.status,
     message: error.message,
